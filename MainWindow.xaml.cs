4eac8cf94bf8614b1aee2bad3d539c36410c62a6
Ahmad K. Abdullah <ahmadkabdullah@pm.me>
2022-03-18 20:56:49 +0300

    Get some NYoutubeDLP functionality into the app

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NYoutubeDL;
+using NYoutubeDL.Models;
 
 namespace GetTube
 {
@@ -20,9 +23,83 @@ namespace GetTube
     /// </summary>
     public partial class MainWindow : Window
     {
+        private YoutubeDLP youtubeDl = new();
+        private DownloadInfo? videoInfo;
+
         public MainWindow()
         {
             InitializeComponent();
+            youtubeDl.YoutubeDlPath = "C:/Users/da/Downloads/ytdl.exe";
+            youtubeDl.Options.GeneralOptions.ExtractorDescriptions = true;
+        }
+
+        private void EventFlaticon(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {"https://www.flaticon.com"}") { CreateNoWindow = true });
+        }
+
+        private void EventSource(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {"https://github.com/ahmadkabdullah/GetTube"}") { CreateNoWindow = true });
+        }
+
+        async private void EventGetVideo(object sender, RoutedEventArgs e)
+        {
+            // reset the text
+            videoInfo = null;
+            varVidInfo.Opacity = 0.2;
+            varStatus.Content = "Fetching video info...";
+            varVidTitle.Text = "Title";
+
+            // get the link
+            youtubeDl.VideoUrl = varVideoURL.Text;
+
+            // get info using the link
+            videoInfo = await youtubeDl.GetDownloadInfoAsync();
+
+            // if info was not found
+            if (videoInfo == null)
+            {
+                varStatus.Content = "Could not retrieve video info...";
+            } 
+            // otherwise when info was found
+            else
+            {
+                varVidInfo.Opacity = 0.9;
+                varVidTitle.Text = videoInfo.Title;
+            }
+        }
+
+        private void DownloadVideoLow(object sender, RoutedEventArgs e)
+        {
+            if (videoInfo == null)
+            {
+                return;
+            }
+        }
+
+        private void DownloadAudio(object sender, RoutedEventArgs e)
+        {
+            if (videoInfo == null)
+            {
+                return;
+            }
+        }
+
+        private void DownloadVideoMedium(object sender, RoutedEventArgs e)
+        {
+            if (videoInfo == null)
+            {
+                return;
+            }
+        }
+
+        private void DownloadVideoHigh(object sender, RoutedEventArgs e)
+        {
+            if (videoInfo == null)
+            {
+                return;
+            }
         }
     }
 }
