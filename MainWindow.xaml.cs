781a8995d24e1ede8478df1b93915e9ff45c47ba
Ahmad K. Abdullah <ahmadkabdullah@pm.me>
2022-03-31 13:17:53 +0300

    Reformat the main file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,292 +23,287 @@ using Syroot.Windows.IO;
 
 namespace GetTube
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        private YoutubeClient youtube = new();
-        private StreamManifest? streamManifest;
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private YoutubeClient youtube = new();
+		private StreamManifest? streamManifest;
 
-        private string? SelectedTheme;
-        private string? SelectedLanguage;
+		private string? SelectedTheme;
+		private string? SelectedLanguage;
 
-        private string? DownloadsFolder;
+		private string? DownloadsFolder;
 
-        public MainWindow()
-        {
-            InitializeComponent();
+		public MainWindow()
+		{
+			InitializeComponent();
 
-            // check for config's existence
-            RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube");
-            if (key == null)
-            {
-                // if first run, create config for reading
-                RegistryKey newKey = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\GetTube", true);
-                newKey.SetValue("Theme", "Light");
-                newKey.SetValue("Language", "en-US");
-                newKey.Close();
-            }
+			// check for config's existence
+			RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube");
+			if (key == null)
+			{
+				// if first run, create config for reading
+				RegistryKey newKey = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\GetTube", true);
+				newKey.SetValue("Theme", "Light");
+				newKey.SetValue("Language", "en-US");
+				newKey.Close();
+			}
 
-            // read the configuration anyways
-            ReadConfig();
+			// read the configuration anyways
+			ReadConfig();
 
-            // modify UI according to config
-            SetUILang(SelectedLanguage);
-            SetUITheme(SelectedTheme);
+			// modify UI according to config
+			SetUILang(SelectedLanguage);
+			SetUITheme(SelectedTheme);
 
-            // set the downloads folder
-            DownloadsFolder = new KnownFolder(KnownFolderType.Downloads).Path;
-        }
+			// set the downloads folder
+			DownloadsFolder = new KnownFolder(KnownFolderType.Downloads).Path;
+		}
 
-        private void ReadConfig()
-        {
-            RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube");
-            SelectedLanguage = key.GetValue("Language").ToString();
-            SelectedTheme = key.GetValue("Theme").ToString();
-            key.Close();
-        }
+		private void ReadConfig()
+		{
+			RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube");
+			SelectedLanguage = key.GetValue("Language").ToString();
+			SelectedTheme = key.GetValue("Theme").ToString();
+			key.Close();
+		}
 
-        private void UpdateConfig()
-        {
-            RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube", true);
-            key.SetValue("Language", SelectedLanguage);
-            key.SetValue("Theme", SelectedTheme);
-            key.Close();
-        }
+		private void UpdateConfig()
+		{
+			RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube", true);
+			key.SetValue("Language", SelectedLanguage);
+			key.SetValue("Theme", SelectedTheme);
+			key.Close();
+		}
 
-        private void EventFlaticon(object sender, RoutedEventArgs e)
-        {
-            secStatus.Content = Properties.Resources.Flaticon;
-        }
+		private void EventFlaticon(object sender, RoutedEventArgs e)
+		{
+			secStatus.Content = Properties.Resources.Flaticon;
+		}
 
-        private void EventSource(object sender, RoutedEventArgs e)
-        {
-            secStatus.Content = Properties.Resources.Github;
-            Process.Start(new ProcessStartInfo("cmd", $"/c start {"https://github.com/ahmadkabdullah/GetTube"}") { CreateNoWindow = true });
-        }
+		private void EventSource(object sender, RoutedEventArgs e)
+		{
+			secStatus.Content = Properties.Resources.Github;
+			Process.Start(new ProcessStartInfo("cmd", $"/c start {"https://github.com/ahmadkabdullah/GetTube"}") { CreateNoWindow = true });
+		}
 
-        // Get the video
-        
-        async private void EventGetVideo(object sender, RoutedEventArgs e)
-        {
-            // delete the info of the previous video
-            ResetVideoInfoUI();
+		// Get the video
 
-            // say fetching and do fetch
-            varStatus.Content = Properties.Resources.Fetching;
+		async private void EventGetVideo(object sender, RoutedEventArgs e)
+		{
+			// delete the info of the previous video
+			ResetVideoInfoUI();
 
-            YoutubeExplode.Videos.Video? video;
+			// say fetching and do fetch
+			varStatus.Content = Properties.Resources.Fetching;
 
-            try
-            {
-                video = await youtube.Videos.GetAsync(varVideoURL.Text);
-            }
-            catch
-            {
-                video = null;
-            }
+			YoutubeExplode.Videos.Video? video;
 
-            // if video not found 
-            if (video == null)
-            {
-                varStatus.Content = Properties.Resources.NotFound;
-            }
-            // otherwise if found
-            else
-            {
-                // say info was found
-                varStatus.Content = Properties.Resources.Found;
+			try
+			{
+				video = await youtube.Videos.GetAsync(varVideoURL.Text);
+			}
+			catch
+			{
+				video = null;
+			}
 
-                // set the values for preview from url
-                varVidTitle.Text = video.Title;
-                varVidAuthor.Text = video.Author.Title;
-                varVidDuration.Text = video.Duration.ToString();
-                
-                // get a list of streams
-                streamManifest = await youtube.Videos.Streams.GetManifestAsync(varVideoURL.Text);
+			// if video not found 
+			if (video == null)
+			{
+				varStatus.Content = Properties.Resources.NotFound;
+			}
 
-                // make buttons clickable
-                videoBtn.Click += DownloadVideo;
-                audioBtn.Click += DownloadAudio;
+			// say info was found
+			varStatus.Content = Properties.Resources.Found;
 
-                // make section appear
-                varVidInfo.Opacity = 0.9;
-                varStatus.Content = Properties.Resources.WaitingFormat;
-            }
-        }
+			// set the values for preview from url
+			varVidTitle.Text = video.Title;
+			varVidAuthor.Text = video.Author.Title;
+			varVidDuration.Text = video.Duration.ToString();
 
-        // Download Options
+			// get a list of streams
+			streamManifest = await youtube.Videos.Streams.GetManifestAsync(varVideoURL.Text);
 
-        async private void DownloadAudio(object sender, RoutedEventArgs e)
-        {
-            // Get audio stream
-            IStreamInfo? streamInfo = streamManifest
-                .GetAudioOnlyStreams()
-                .GetWithHighestBitrate();
+			// make buttons clickable
+			videoBtn.Click += DownloadVideo;
+			audioBtn.Click += DownloadAudio;
 
-            // Stop button from working
-            varStatus.Content = Properties.Resources.Downloading;
-            audioBtn.Click -= DownloadAudio;
+			// make section appear
+			varVidInfo.Opacity = 0.9;
+			varStatus.Content = Properties.Resources.WaitingFormat;
+		}
 
-            string cleanedTitle = Utility.RemoveSpecialCharacters(varVidTitle.Text);
-            string fileAndPath = DownloadsFolder+"\\"+$"{cleanedTitle}.{streamInfo.Container}";
+		// Download Options
 
-            // Download and notify after
-            await youtube.Videos.Streams.DownloadAsync(streamInfo, fileAndPath);
-            varStatus.Content = Properties.Resources.DownloadedAudio;
-        }
+		async private void DownloadAudio(object sender, RoutedEventArgs e)
+		{
+			// Get audio stream
+			IStreamInfo? streamInfo = streamManifest
+				.GetAudioOnlyStreams()
+				.GetWithHighestBitrate();
 
-        async private void DownloadVideo(object sender, RoutedEventArgs e)
-        {
-            // Get mixed stream 
-            IStreamInfo? streamInfo = streamManifest
-                .GetMuxedStreams()
-                .GetWithHighestVideoQuality();
+			// Stop button from working
+			varStatus.Content = Properties.Resources.Downloading;
+			audioBtn.Click -= DownloadAudio;
 
-            // Stop button from working
-            varStatus.Content = Properties.Resources.Downloading;
-            videoBtn.Click -= DownloadVideo;
+			string cleanedTitle = Utility.RemoveSpecialCharacters(varVidTitle.Text);
+			string fileAndPath = DownloadsFolder + "\\" + $"{cleanedTitle}.{streamInfo.Container}";
 
-            string cleanedTitle = Utility.RemoveSpecialCharacters(varVidTitle.Text);
-            string fileAndPath = DownloadsFolder+"\\"+$"{cleanedTitle}.{streamInfo.Container}";
-
-            // Download and notify after
+			// Download and notify after
 			await youtube.Videos.Streams.DownloadAsync(streamInfo, fileAndPath);
-            varStatus.Content = Properties.Resources.DownloadedVideo;
-        }
+			varStatus.Content = Properties.Resources.DownloadedAudio;
+		}
 
-        // Pressing Language and Color Buttons
+		async private void DownloadVideo(object sender, RoutedEventArgs e)
+		{
+			// Get mixed stream 
+			IStreamInfo? streamInfo = streamManifest
+				.GetMuxedStreams()
+				.GetWithHighestVideoQuality();
 
-        private void EventLang(object sender, RoutedEventArgs e)
-        {
-            switch (SelectedLanguage)
-            {
-                case "en-US":
-                    SetUILang("ar-IQ");
-                    secStatus.Content = Properties.Resources.ToKurdish;
-                    break;
-                case "ar-IQ":
-                    SetUILang("en-US");
-                    secStatus.Content = Properties.Resources.ToEnglish;
-                    break;
-            }
-        }
+			// Stop button from working
+			varStatus.Content = Properties.Resources.Downloading;
+			videoBtn.Click -= DownloadVideo;
 
-        private void EventColor(object sender, RoutedEventArgs e)
-        {
-            // switch theme
-            switch (SelectedTheme)
-            {
-                case "Dark":
-                    SetUITheme("Light");
-                    secStatus.Content = Properties.Resources.ToLight;
-                    break;
-                case "Light":
-                    SetUITheme("Dark");
-                    secStatus.Content = Properties.Resources.ToDark;
-                    break;
-            }
-        }
+			string cleanedTitle = Utility.RemoveSpecialCharacters(varVidTitle.Text);
+			string fileAndPath = DownloadsFolder + "\\" + $"{cleanedTitle}.{streamInfo.Container}";
 
-        private void SetUILang(string language)
-        {
-            // set another language
-            switch (language)
-            {
-                case "ar-IQ":
-                    varVidAuthor.FontFamily =
-                        varVidDuration.FontFamily =
-                        varVidTitle.FontFamily =
-                        videoBtn.FontFamily =
-                        audioBtn.FontFamily = 
-                        secStatus.FontFamily =
-                        varStatus.FontFamily = new FontFamily("NRT BOLD");
-                    SelectedLanguage = "ar-IQ";
-                        break;
+			// Download and notify after
+			await youtube.Videos.Streams.DownloadAsync(streamInfo, fileAndPath);
+			varStatus.Content = Properties.Resources.DownloadedVideo;
+		}
 
-                case "en-US":
-                    varVidAuthor.FontFamily =
-                        varVidDuration.FontFamily =
-                        varVidTitle.FontFamily =
-                        videoBtn.FontFamily =
-                        audioBtn.FontFamily =
-                        secStatus.FontFamily =
-                        varStatus.FontFamily = new FontFamily("Fira Sans");
-                    SelectedLanguage = "en-US";
-                        break;
-            }
+		// Pressing Language and Color Buttons
 
-            // switch to set language
-            LocalizeDictionary.Instance.Culture = new CultureInfo(SelectedLanguage);
+		private void EventLang(object sender, RoutedEventArgs e)
+		{
+			switch (SelectedLanguage)
+			{
+				case "en-US":
+					SetUILang("ar-IQ");
+					secStatus.Content = Properties.Resources.ToKurdish;
+					break;
+				case "ar-IQ":
+					SetUILang("en-US");
+					secStatus.Content = Properties.Resources.ToEnglish;
+					break;
+			}
+		}
 
-            // update configuration
-            UpdateConfig();
-        }
+		private void EventColor(object sender, RoutedEventArgs e)
+		{
+			// switch theme
+			switch (SelectedTheme)
+			{
+				case "Dark":
+					SetUITheme("Light");
+					secStatus.Content = Properties.Resources.ToLight;
+					break;
+				case "Light":
+					SetUITheme("Dark");
+					secStatus.Content = Properties.Resources.ToDark;
+					break;
+			}
+		}
 
-        private void SetUITheme(string theme)
-        {
-            // used colors
-            Brush bgCol, fgCol, fgColB;
+		private void SetUILang(string language)
+		{
+			// set another language
+			switch (language)
+			{
+				case "ar-IQ":
+					varVidAuthor.FontFamily =
+						varVidDuration.FontFamily =
+						varVidTitle.FontFamily =
+						videoBtn.FontFamily =
+						audioBtn.FontFamily =
+						secStatus.FontFamily =
+						varStatus.FontFamily = new FontFamily("NRT BOLD");
+					SelectedLanguage = "ar-IQ";
+					break;
 
-            // switch theme
-            switch (theme)
-            {
-                default:
-                case "Light":
-                    // light theme colors
-                    bgCol = Brushes.WhiteSmoke;
-                    fgCol = new SolidColorBrush(Color.FromArgb(0xFF, 20, 20, 20));
-                    fgColB = Brushes.Gray;
-                    SelectedTheme = "Light";
-                    break;
-                case "Dark":
-                    // dark theme colors
-                    bgCol = new SolidColorBrush(Color.FromArgb(0xFF, 30, 30, 30));
-                    fgCol = Brushes.WhiteSmoke;
-                    fgColB = Brushes.Gray;
-                    SelectedTheme = "Dark";
-                    break;
-            }
+				case "en-US":
+					varVidAuthor.FontFamily =
+						varVidDuration.FontFamily =
+						varVidTitle.FontFamily =
+						videoBtn.FontFamily =
+						audioBtn.FontFamily =
+						secStatus.FontFamily =
+						varStatus.FontFamily = new FontFamily("Fira Sans");
+					SelectedLanguage = "en-US";
+					break;
+			}
 
-            // finally set all the colors
-            container.Background = bgCol;
-            varVideoURL.Background = bgCol;
-            varVideoURL.Foreground = fgCol;
-            varStatus.Foreground = fgCol;
-            secStatus.Foreground = fgColB;
+			// switch to set language
+			LocalizeDictionary.Instance.Culture = new CultureInfo(SelectedLanguage);
 
-            // update configuration
-            UpdateConfig();
-        }
+			// update configuration
+			UpdateConfig();
+		}
 
-        // to be run after a new link is given
-        private void ResetVideoInfoUI()
-        {
-            varVidInfo.Opacity = 0.2;
-            varVidTitle.Text = Properties.Resources.VidTitle;
-            varVidAuthor.Text = Properties.Resources.VidAuthor;
-            varVidDuration.Text = Properties.Resources.VidDuration;
+		private void SetUITheme(string theme)
+		{
+			// used colors
+			Brush bgCol, fgCol, fgColB;
 
-            // make buttons not clickable
-            videoBtn.Click -= DownloadVideo;
-            audioBtn.Click -= DownloadAudio;
-        }
+			// switch theme
+			switch (theme)
+			{
+				default:
+				case "Light":
+					// light theme colors
+					bgCol = Brushes.WhiteSmoke;
+					fgCol = new SolidColorBrush(Color.FromArgb(0xFF, 20, 20, 20));
+					fgColB = Brushes.Gray;
+					SelectedTheme = "Light";
+					break;
+				case "Dark":
+					// dark theme colors
+					bgCol = new SolidColorBrush(Color.FromArgb(0xFF, 30, 30, 30));
+					fgCol = Brushes.WhiteSmoke;
+					fgColB = Brushes.Gray;
+					SelectedTheme = "Dark";
+					break;
+			}
 
-    }
+			// finally set all the colors
+			container.Background = bgCol;
+			varVideoURL.Background = bgCol;
+			varVideoURL.Foreground = fgCol;
+			varStatus.Foreground = fgCol;
+			secStatus.Foreground = fgColB;
+
+			// update configuration
+			UpdateConfig();
+		}
+
+		// to be run after a new link is given
+		private void ResetVideoInfoUI()
+		{
+			varVidInfo.Opacity = 0.2;
+			varVidTitle.Text = Properties.Resources.VidTitle;
+			varVidAuthor.Text = Properties.Resources.VidAuthor;
+			varVidDuration.Text = Properties.Resources.VidDuration;
+
+			// make buttons not clickable
+			videoBtn.Click -= DownloadVideo;
+			audioBtn.Click -= DownloadAudio;
+		}
+
+	}
 }
 
 class Utility
 {
-    static public string RemoveSpecialCharacters(string input)
+	static public string RemoveSpecialCharacters(string input)
 	{
-        string output = input;
-        var charsToRemove = new string[] { ",", "'", "\"" };
-        foreach (var c in charsToRemove)
-        {
-            output = output.Replace(c, string.Empty);
-        }
-        return output;
+		string output = input;
+		var charsToRemove = new string[] { ",", "'", "\"" };
+		foreach (var c in charsToRemove)
+			output = output.Replace(c, string.Empty);
+		return output;
 	}
 }
