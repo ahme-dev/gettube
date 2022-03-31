7ba6c409149616a40a696d0ea67b13a01646902e
Ahmad K. Abdullah <ahmadkabdullah@pm.me>
2022-03-31 15:43:54 +0300

    Refactor code and unify ui element names

---
+++ Elements.cs
@@ -0,0 +1,24 @@
+﻿namespace GetTube
+{
+	partial class Ui
+	{
+		public static struct Elements
+		{
+			object container;
+			object status;
+			object secStatus;
+			object urlBox;
+			struct vid
+			{
+				object box;
+				object title;
+				object author;
+				object duration;
+				object thumbnail;
+			}
+			object buttonVideo;
+			object buttonAudio;
+		}
+	}
+
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,234 +20,218 @@ using Microsoft.Win32;
 using Syroot.Windows.IO;
 using YoutubeDLSharp;
 
-namespace GetTube
+namespace GetTube;
+
+public partial class MainWindow : Window
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	private string? SelectedTheme;
+	private string? SelectedLanguage;
+	private string? DownloadsFolder;
+
+	public MainWindow()
 	{
-		private string? SelectedTheme;
-		private string? SelectedLanguage;
+		InitializeComponent();
 
-		private string? DownloadsFolder;
+		// deal with configuration
+		ReadConfig();
 
-		public MainWindow()
+		// modify UI according to config
+		SetUILang(SelectedLanguage);
+		SetUITheme(SelectedTheme);
+
+		// set the downloads folder
+		DownloadsFolder = new KnownFolder(KnownFolderType.Downloads).Path;
+	}
+
+	// Configuration management
+
+	private void ReadConfig()
+	{
+		RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube");
+		if (key == null)
 		{
-			InitializeComponent();
-
-			// check for config's existence
-			RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube");
-			if (key == null)
-			{
-				// if first run, create config for reading
-				RegistryKey newKey = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\GetTube", true);
-				newKey.SetValue("Theme", "Light");
-				newKey.SetValue("Language", "en-US");
-				newKey.Close();
-			}
-
-			// read the configuration anyways
+			CreateConfig();
 			ReadConfig();
-
-			// modify UI according to config
-			SetUILang(SelectedLanguage);
-			SetUITheme(SelectedTheme);
-
-			// set the downloads folder
-			DownloadsFolder = new KnownFolder(KnownFolderType.Downloads).Path;
+			return;
 		}
+		SelectedLanguage = key.GetValue("Language").ToString();
+		SelectedTheme = key.GetValue("Theme").ToString();
+		key.Close();
+	}
+	private void CreateConfig()
+	{
+		RegistryKey newKey = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\GetTube", true);
+		newKey.SetValue("Theme", "Light");
+		newKey.SetValue("Language", "en-US");
+		newKey.Close();
+	}
 
-		private void ReadConfig()
+	private void UpdateConfig()
+	{
+		RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube", true);
+		key.SetValue("Language", SelectedLanguage);
+		key.SetValue("Theme", SelectedTheme);
+		key.Close();
+	}
+
+	// Source and Thanks Buttons
+
+	private void EventFlaticon(object sender, RoutedEventArgs e)
+	{
+		uiSecStatus.Content = Properties.Resources.Flaticon;
+	}
+
+	private void EventSource(object sender, RoutedEventArgs e)
+	{
+		uiSecStatus.Content = Properties.Resources.Github;
+		Process.Start(new ProcessStartInfo("cmd", $"/c start {"https://github.com/ahmadkabdullah/GetTube"}") { CreateNoWindow = true });
+	}
+
+	// Fetching Info And Downloading 
+
+	async private void EventGetVideo(object sender, RoutedEventArgs e)
+	{
+		// delete the info of the previous video
+		ResetVideoInfoUI();
+
+		// say fetching and do fetch
+		uiStatus.Content = Properties.Resources.Fetching;
+
+		// TODO fetch video info
+
+		// say info was found
+		uiStatus.Content = Properties.Resources.Found;
+
+		// TODO set video info on ui
+
+		// make buttons clickable
+		uiVideoBtn.Click += DownloadVideo;
+		uiAudioBtn.Click += DownloadAudio;
+
+		// make section appear
+		uiVidBox.Opacity = 0.9;
+		uiStatus.Content = Properties.Resources.WaitingFormat;
+	}
+
+	async private void DownloadAudio(object sender, RoutedEventArgs e)
+	{
+		// TODO download the audio
+
+		uiStatus.Content = Properties.Resources.DownloadedAudio;
+	}
+
+	async private void DownloadVideo(object sender, RoutedEventArgs e)
+	{
+		// TODO download the audio
+
+		uiStatus.Content = Properties.Resources.DownloadedVideo;
+	}
+
+	// Language and Color Buttons
+
+	private void EventLang(object sender, RoutedEventArgs e)
+	{
+		if (SelectedLanguage == "en-US")
+			SetUILang("ar-IQ");
+		else
+			SetUILang("en-US");
+	}
+
+	private void EventColor(object sender, RoutedEventArgs e)
+	{
+		if (SelectedTheme == "Dark")
+			SetUITheme("Light");
+		else
+			SetUITheme("Dark");
+	}
+
+	// Changing the UI
+
+	private void SetUILang(string language)
+	{
+		if (language == "ar-IQ")
 		{
-			RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube");
-			SelectedLanguage = key.GetValue("Language").ToString();
-			SelectedTheme = key.GetValue("Theme").ToString();
-			key.Close();
+			uiVidAuthor.FontFamily =
+				uiVidDuration.FontFamily =
+				uiVidTitle.FontFamily =
+				uiVideoBtn.FontFamily =
+				uiAudioBtn.FontFamily =
+				uiSecStatus.FontFamily =
+				uiStatus.FontFamily = new FontFamily("NRT BOLD");
+			SelectedLanguage = "ar-IQ";
+			uiSecStatus.Content = Properties.Resources.ToKurdish;
 		}
-
-		private void UpdateConfig()
+		else
 		{
-			RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\GetTube", true);
-			key.SetValue("Language", SelectedLanguage);
-			key.SetValue("Theme", SelectedTheme);
-			key.Close();
+			uiVidAuthor.FontFamily =
+				uiVidDuration.FontFamily =
+				uiVidTitle.FontFamily =
+				uiVideoBtn.FontFamily =
+				uiAudioBtn.FontFamily =
+				uiSecStatus.FontFamily =
+				uiStatus.FontFamily = new FontFamily("Fira Sans");
+			SelectedLanguage = "en-US";
+			uiSecStatus.Content = Properties.Resources.ToEnglish;
 		}
 
-		private void EventFlaticon(object sender, RoutedEventArgs e)
+		// switch to set language
+		LocalizeDictionary.Instance.Culture = new CultureInfo(SelectedLanguage);
+
+		// update configuration
+		UpdateConfig();
+	}
+
+	private void SetUITheme(string theme)
+	{
+		// used colors
+		Brush bgCol, fgCol, fgColB;
+
+		if (theme == "Light")
 		{
-			secStatus.Content = Properties.Resources.Flaticon;
-		}
 
-		private void EventSource(object sender, RoutedEventArgs e)
+			// light theme colors
+			bgCol = Brushes.WhiteSmoke;
+			fgCol = new SolidColorBrush(Color.FromArgb(0xFF, 20, 20, 20));
+			fgColB = Brushes.Gray;
+			SelectedTheme = "Light";
+			uiSecStatus.Content = Properties.Resources.ToLight;
+		}
+		else
 		{
-			secStatus.Content = Properties.Resources.Github;
-			Process.Start(new ProcessStartInfo("cmd", $"/c start {"https://github.com/ahmadkabdullah/GetTube"}") { CreateNoWindow = true });
+			// dark theme colors
+			bgCol = new SolidColorBrush(Color.FromArgb(0xFF, 30, 30, 30));
+			fgCol = Brushes.WhiteSmoke;
+			fgColB = Brushes.Gray;
+			SelectedTheme = "Dark";
+			uiSecStatus.Content = Properties.Resources.ToDark;
 		}
 
-		// Get the video
+		// finally set all the colors
+		uiContainer.Background = bgCol;
+		uiURLBox.Background = bgCol;
+		uiURLBox.Foreground = fgCol;
+		uiStatus.Foreground = fgCol;
+		uiSecStatus.Foreground = fgColB;
 
-		async private void EventGetVideo(object sender, RoutedEventArgs e)
-		{
-			// delete the info of the previous video
-			ResetVideoInfoUI();
+		// update configuration
+		UpdateConfig();
+	}
 
-			// say fetching and do fetch
-			varStatus.Content = Properties.Resources.Fetching;
+	private void ResetVideoInfoUI()
+	{
+		// reset elements after new link is given
+		uiVidBox.Opacity = 0.2;
+		uiVidTitle.Text = Properties.Resources.VidTitle;
+		uiVidAuthor.Text = Properties.Resources.VidAuthor;
+		uiVidDuration.Text = Properties.Resources.VidDuration;
 
-			// TODO fetch video info
-
-			// say info was found
-			varStatus.Content = Properties.Resources.Found;
-
-			// TODO set video info on ui
-
-			// make buttons clickable
-			videoBtn.Click += DownloadVideo;
-			audioBtn.Click += DownloadAudio;
-
-			// make section appear
-			varVidInfo.Opacity = 0.9;
-			varStatus.Content = Properties.Resources.WaitingFormat;
-		}
-
-		// Download Options
-
-		async private void DownloadAudio(object sender, RoutedEventArgs e)
-		{
-			// TODO download the audio
-
-			varStatus.Content = Properties.Resources.DownloadedAudio;
-		}
-
-		async private void DownloadVideo(object sender, RoutedEventArgs e)
-		{
-			// TODO download the audio
-
-			varStatus.Content = Properties.Resources.DownloadedVideo;
-		}
-
-		// Pressing Language and Color Buttons
-
-		private void EventLang(object sender, RoutedEventArgs e)
-		{
-			switch (SelectedLanguage)
-			{
-				case "en-US":
-					SetUILang("ar-IQ");
-					secStatus.Content = Properties.Resources.ToKurdish;
-					break;
-				case "ar-IQ":
-					SetUILang("en-US");
-					secStatus.Content = Properties.Resources.ToEnglish;
-					break;
-			}
-		}
-
-		private void EventColor(object sender, RoutedEventArgs e)
-		{
-			// switch theme
-			switch (SelectedTheme)
-			{
-				case "Dark":
-					SetUITheme("Light");
-					secStatus.Content = Properties.Resources.ToLight;
-					break;
-				case "Light":
-					SetUITheme("Dark");
-					secStatus.Content = Properties.Resources.ToDark;
-					break;
-			}
-		}
-
-		private void SetUILang(string language)
-		{
-			// set another language
-			switch (language)
-			{
-				case "ar-IQ":
-					varVidAuthor.FontFamily =
-						varVidDuration.FontFamily =
-						varVidTitle.FontFamily =
-						videoBtn.FontFamily =
-						audioBtn.FontFamily =
-						secStatus.FontFamily =
-						varStatus.FontFamily = new FontFamily("NRT BOLD");
-					SelectedLanguage = "ar-IQ";
-					break;
-
-				case "en-US":
-					varVidAuthor.FontFamily =
-						varVidDuration.FontFamily =
-						varVidTitle.FontFamily =
-						videoBtn.FontFamily =
-						audioBtn.FontFamily =
-						secStatus.FontFamily =
-						varStatus.FontFamily = new FontFamily("Fira Sans");
-					SelectedLanguage = "en-US";
-					break;
-			}
-
-			// switch to set language
-			LocalizeDictionary.Instance.Culture = new CultureInfo(SelectedLanguage);
-
-			// update configuration
-			UpdateConfig();
-		}
-
-		private void SetUITheme(string theme)
-		{
-			// used colors
-			Brush bgCol, fgCol, fgColB;
-
-			// switch theme
-			switch (theme)
-			{
-				default:
-				case "Light":
-					// light theme colors
-					bgCol = Brushes.WhiteSmoke;
-					fgCol = new SolidColorBrush(Color.FromArgb(0xFF, 20, 20, 20));
-					fgColB = Brushes.Gray;
-					SelectedTheme = "Light";
-					break;
-				case "Dark":
-					// dark theme colors
-					bgCol = new SolidColorBrush(Color.FromArgb(0xFF, 30, 30, 30));
-					fgCol = Brushes.WhiteSmoke;
-					fgColB = Brushes.Gray;
-					SelectedTheme = "Dark";
-					break;
-			}
-
-			// finally set all the colors
-			container.Background = bgCol;
-			varVideoURL.Background = bgCol;
-			varVideoURL.Foreground = fgCol;
-			varStatus.Foreground = fgCol;
-			secStatus.Foreground = fgColB;
-
-			// update configuration
-			UpdateConfig();
-		}
-
-		// to be run after a new link is given
-		private void ResetVideoInfoUI()
-		{
-			varVidInfo.Opacity = 0.2;
-			varVidTitle.Text = Properties.Resources.VidTitle;
-			varVidAuthor.Text = Properties.Resources.VidAuthor;
-			varVidDuration.Text = Properties.Resources.VidDuration;
-
-			// make buttons not clickable
-			videoBtn.Click -= DownloadVideo;
-			audioBtn.Click -= DownloadAudio;
-		}
-
+		// make buttons not clickable
+		uiVideoBtn.Click -= DownloadVideo;
+		uiAudioBtn.Click -= DownloadAudio;
 	}
 }
 
-class Utility
+class GTUtility
 {
 	static public string RemoveSpecialCharacters(string input)
 	{
@@ -255,6 +239,7 @@ class Utility
 		var charsToRemove = new string[] { ",", "'", "\"" };
 		foreach (var c in charsToRemove)
 			output = output.Replace(c, string.Empty);
+
 		return output;
 	}
 }
